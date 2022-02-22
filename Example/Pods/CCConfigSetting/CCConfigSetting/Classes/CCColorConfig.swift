0203d90d80460a2c884c14c0b029c5b044da1e39
VE66 <942914231@qq.com>
2022-02-22 17:51:49 +0800

    34

---
+++ Example/Pods/CCConfigSetting/CCConfigSetting/Classes/CCColorConfig.swift
@@ -0,0 +1,288 @@
+//
+//  CCColorConfig.swift
+//  CCConfigSetting
+//
+//  Created by ZCZ on 2022/2/22.
+//
+
+import UIKit
+
+// MARK: - App 主色调
+//public let MGColor_Main = RGBValueColor(hex: "#39bdbf") // 导航颜色 RGB(57,189,191)
+public let MGColor_Main = RGBValueColor(hex: "#459AE9") // 导航颜色 RGB(69,154,233)
+
+
+// MARK: - 背景色
+
+public let MGColor_FAFAFC = RGBValueColor(hex: "#FAFAFC") // 版本背景颜色 RGB(250,250,252)
+public let MGColor_090734 = RGBValueColor(hex:"#090734") //通话软电话背景颜色 RGB(9,7,52)
+
+
+// 深色模式下的颜色
+
+public let MGColor_F0F0F5 = RGBValueColor(hex: "#f0f0f5") //240,240,245
+
+/// 49,50,51 黑
+public let MGColor_313233 = RGBValueColor(hex: "#313233")
+
+public let MGColor_282829 = RGBValueColor(hex: "#282829") // 40,40,41
+public let MGColor_1E1E1F = RGBValueColor(hex: "#1E1E1F") // 30,30,31
+public let MGColor_14 = RGBValueColor(hex: "#141414") // 20,20,20
+
+/// 深黑 15, 15, 15
+public let MGColor_0F = RGBValueColor(hex: "#0F0F0F") // 15,15,15
+///  68, 68, 71  黑
+public let MGColor_444447 = RGBValueColor(hex: "#444447") // 68,68,71 line黑
+/// 灰黑 74,75,77
+public let MGColor_4A4B4D = RGBValueColor(hex: "#4A4B4D") //74,75,77 line黑2
+
+/// 亮黑 25, 25, 26
+public let MGColor_19191A = RGBValueColor(hex: "#19191A")
+
+
+/// 245, 245, 245
+public let QMColor_F5 = RGBValueColor(hex: "#F5F5F5") //(245, 245, 245, 1)
+
+
+// MARK: - 警示颜色-(255,220,220)
+public let MGColor_FFDCDC = RGBValueColor(hex: "#FFDCDC")
+
+// MARK: - 字体颜色
+public let MGColor_00 = RGBValueColor(hex: "#000000")
+/// 黑 26， 26， 26
+public let MGColor_1A = RGBValueColor(hex: "#1A1A1A")
+/// 黑色--(51,51,51)
+public let MGColor_33 = RGBValueColor(hex: "#333333")
+/// 颜色--77,77,77
+public let MGColor_4D = RGBValueColor(hex: "#4D4D4D")
+/// 深灰--102,102,102
+public let MGColor_66 = RGBValueColor(hex: "#666666")
+/// 115,115,115 黑灰
+public let MGColor_73 = RGBValueColor(hex: "#737373")
+/// 灰--(153,153,153)
+public let MGColor_99 = RGBValueColor(hex: "#999999")
+/// 灰(179,179,179)
+public let MGColor_B3 = RGBValueColor(hex: "#B3B3B3")
+/// 灰=(191,191,191)
+public let MGColor_BF = RGBValueColor(hex: "#BFBFBF")
+/// 浅灰(204, 204, 204)
+public let MGColor_CC = RGBValueColor(hex: "#CCCCCC")
+/// 颜色(211, 211, 211)
+public let MGColor_D3 = RGBValueColor(hex: "#D3D3D3")
+/// 颜色(216, 216, 216)
+public let MGColor_D8 = RGBValueColor(hex: "#D8D8D8")
+/// 颜色(217, 217, 217)
+public let MGColor_D9 = RGBValueColor(hex: "#D9D9D9")
+/// 灰色 (222,222,222)
+public let MGColor_DE = RGBValueColor(hex: "#DEDEDE")
+/// 颜色230, 230, 230
+public let MGColor_E6 = RGBValueColor(hex: "#E6E6E6")
+/// 颜色235, 235, 235
+public let MGColor_EB = RGBValueColor(hex: "#EBEBEB")
+/// 灰色 (240,240,240)
+public let MGColor_F0 = RGBValueColor(hex: "#F0F0F0")
+/// 背景色 (242,242,242)
+public let MGColor_F2 = RGBValueColor(hex: "#f2f2f2")
+/// 背景色 (247,247,247)
+public let MGColor_F7 = RGBValueColor(hex: "#f7f7f7")
+/// 背景色(248,248,248)
+public let MGColor_F8 = RGBValueColor(hex: "#f8f8f8")
+
+
+
+/// 褐色 RGB(186,78,78)
+public let MGColor_BA4E4E = RGBValueColor(hex: "#BA4E4E")
+
+
+
+
+/// 绿色 (27,187,158)
+public let MGColor_green_1BBB9E = RGBValueColor(hex: "#1BBB9E")
+/// 绿色 (124,205,209)
+public let MGColor_green_7CCDD1 = RGBValueColor(hex: "##7CCDD1")
+/// 青色 43,178,76
+public let MGColor_2BB24C = RGBValueColor(hex: "#2BB24C") // 43, 178, 76
+/// 深绿色 (6, 128, 36)
+public let MGColor_green_068024 = RGBValueColor(hex: "#068024")
+/// 灰绿色 (104,145,149)
+public let MGColor_green_689195 = RGBValueColor(hex: "#689195")
+/// 灰色绿 (179,195,195)
+public let MGColor_green_B3C3C3 = RGBValueColor(hex: "#b3c3c3")
+/// 绿色 (21,200,209)
+public let MGColor_green_15C8D1 = RGBValueColor(hex: "#15c8d1")
+/// 浅绿色 RGB(121,176,93)
+public let MGColor_green_79B05D = RGBValueColor(hex: "#79b05d")
+/// 绿色 RGB(26,187,156)
+public let MGColor_green_1ABB9C = RGBValueColor(hex: "#1abb9c")
+
+
+
+
+/// 紫色 RGB(211,152,233)
+public let MGColor_violet_D398E9 = RGBValueColor(hex: "#D398E9")
+/// 紫色 RGB(211,152,232)
+public let MGColor_violet_D398E8 = RGBValueColor(hex: "#D398E8")
+
+
+/// 灰色 RGB(203,204,205)
+public let MGColor_gray_CBCCCD = RGBValueColor(hex: "#CBCCCD")
+/// 淡褐色-深色
+public let MGColor_B3A594 = RGBValueColor(hex: "#B3A594")
+
+
+/// 黄色 RGB(246,203,1)
+public let MGColor_yellow_F6CB01 = RGBValueColor(hex: "#F6CB01")
+/// 黄色 RGB(248,203,0)
+public let MGColor_yellow_F8CB00 = RGBValueColor(hex: "#F8CB00")
+
+
+/// 橙色 RGB(244,140,77)
+public let MGColor_orange_F48C4D = RGBValueColor(hex: "#f48c4d")
+/// 橙色
+public let MGColor_orange_FF9100 = RGBValueColor(hex: "#FF9100")
+/// 深色模式橙色
+public let MGColor_orange_A66106 = RGBValueColor(hex: "#A66106")
+/// 淡橙色
+public let MGColor_orange_FEF6EC = RGBValueColor(hex: "#FEF6EC")
+/// 橙色 (255, 132, 3)
+public let MGColor_orange_FF8403 = RGBValueColor(hex: "#FF8403")
+/// 橙色 RGB(245,151,0)
+public let MGColor_orange_F59700 = RGBValueColor(hex: "#f59700")
+
+
+
+
+/// 蓝色 RGB(112,189,224)
+public let MGColor_blue_3687D9 = RGBValueColor(hex: "#3687D9")
+/// 蓝色 RGB(101,195,223)
+public let MGColor_blue_65C3DF = RGBValueColor(hex: "#65c3df")
+/// 蓝色 RGB(0,159,227)
+public let MGColor_blue_009fe3 = RGBValueColor(hex: "#009fe3")
+/// 蓝色 186,235,238
+public let MGColor_blue_BAEBEE = RGBValueColor(hex: "#baebee")
+/// 蓝色 RGB(98,177,222)
+public let MGColor_blue_62B1DE = RGBValueColor(hex: "#62B1DE")
+/// 背景淡蓝色 240, 248, 255
+public let MGColor_blue_F0F8FF = RGBValueColor(hex: "#F0F8FF")
+/// 蓝色 20, 130, 255
+public let MGColor_blue_1482FF = RGBValueColor(hex: "#1482FF")
+/// 黑蓝-深 20,79,204
+public let MGColor_blue_144FCC = RGBValueColor(hex: "#144FCC")
+/// 淡蓝 80,130,225
+public let MGColor_blue_5082E1 = RGBValueColor(hex: "#5082E1")
+/// 蓝色-深 22,122,217
+public let MGColor_blue_167AD9 = RGBValueColor(hex: "#167AD9")
+/// 蓝色 69,154,233
+public let MGColor_blue_459AE9 = RGBValueColor(hex: "#459AE9")
+/// 蓝灰 18,101,179
+public let MGColor_blue_1265B3 = RGBValueColor(hex: "#1265B3")
+
+
+/// 红色 RGB(250,108,107)
+public let MGColor_red_FA6C6B = RGBValueColor(hex: "#FA6C6B")
+/// 背景淡红色 255, 245, 245
+public let MGColor_red_FFF5F5 = RGBValueColor(hex: "#FFF5F5")
+/// 鲜红色 254,83,76
+public let MGColor_red_FE534C = RGBValueColor(hex: "#FE534C")
+/// 红色 RGB(255,59,48)
+public let MGColor_Label_Red = RGBValueColor(hex: "#FF3B30")
+/// 亮红 255,59,48
+public let MGColor_red_FF3B30 = RGBValueColor(hex: "#FF3B30")
+/// 179, 18, 9 暗红
+public let MGColor_red_B31209 = RGBValueColor(hex: "#B31209")
+
+// MARK: - 蒙版
+public let MGColor_Cover = RGBColor(r: 0, g: 0, b: 0, a: 0.2)
+public let MGColor_Cover_Unallowed = RGBValueColor(hex: "#F5F7FA") // 不可修改  RGB(245,247,250)
+
+public let MColor_l_000 = isDarkStyle ? MGColor_BF : MGColor_00
+public let MColor_l_333 = isDarkStyle ? MGColor_73 : MGColor_33
+public let MColor_l_666 = isDarkStyle ? MGColor_99 : MGColor_66
+public let MColor_l_999 = isDarkStyle ? MGColor_73 : MGColor_99
+public let MColor_l_459AE9 = MGColor_blue_459AE9
+public let MColor_l_5082E1 = isDarkStyle ? MGColor_blue_144FCC : MGColor_blue_5082E1
+public let MColor_l_2BB24C = isDarkStyle ? MGColor_green_068024 : MGColor_2BB24C
+public let MColor_l_FF8403 = isDarkStyle ? MGColor_orange_A66106 : MGColor_orange_FF8403
+
+
+
+// MARK: - 十六进制颜色
+public func RGBValueColor(hex: String, alpha: CGFloat = 1.0) ->UIColor {
+    var cString = hex.trimmingCharacters(in: .whitespacesAndNewlines) as NSString
+    
+    if cString.hasPrefix("#") {
+        cString = cString.substring(from: 1) as NSString
+    }else {
+        return UIColor.white
+    }
+    
+    let rString = cString.substring(with: NSRange(location: 0, length: 2))
+    let gString = cString.substring(with: NSRange(location: 2, length: 2))
+    let bString = cString.substring(with: NSRange(location: 4, length: 2))
+    
+    var r: CUnsignedInt = 0
+    var g: CUnsignedInt = 0
+    var b: CUnsignedInt = 0
+    
+    Scanner(string: rString).scanHexInt32(&r)
+    Scanner(string: gString).scanHexInt32(&g)
+    Scanner(string: bString).scanHexInt32(&b)
+    
+    return UIColor(red: CGFloat(r)/255.0, green: CGFloat(g)/255.0, blue: CGFloat(b)/255.0, alpha: alpha)
+}
+
+// MARK: - RGB颜色
+public func RGBColor(r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat) ->UIColor{
+    return UIColor(red: r/255.0, green: g/255.0, blue: b/255.0, alpha: a)
+}
+
+// MARK: - 随机色
+public func RandColor() ->UIColor {
+    return RGBColor(r: CGFloat(arc4random_uniform(255)), g: CGFloat(arc4random_uniform(255)), b: CGFloat(arc4random_uniform(255)), a: 1)
+}
+
+// MARK: - 背景色返回图片
+extension UIImage {
+    class func imageFromColor(color: UIColor, viewSize: CGSize) -> UIImage{
+        let rect: CGRect = CGRect(x: 0, y: 0, width: viewSize.width, height: viewSize.height)
+        UIGraphicsBeginImageContext(rect.size)
+        let context: CGContext = UIGraphicsGetCurrentContext()!
+        context.setFillColor(color.cgColor)
+        context.fill(rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsGetCurrentContext()
+        return image!
+    }
+    
+    class func imageFromColor(color: UIColor) -> UIImage{
+        let size: CGSize = CGSize(width: 1, height: 1)
+        return self.imageFromColor(color: color, viewSize: size)
+    }
+    
+    // MARK: - 质量压缩
+//    class func compressImageQuality(_ image: UIImage, toByte maxLength: Int) -> Data {
+//        var compression: CGFloat = 1
+//        
+//        guard var data = image.jpegData(compressionQuality: compression),
+//            data.count > maxLength else { return image.jpegData(compressionQuality: compression)! }
+//        
+//        var max: CGFloat = 1
+//        var min: CGFloat = 0
+//        for _ in 0..<6 {
+//            compression = (max + min) / 2
+//            data = image.jpegData(compressionQuality: compression)!
+//            if CGFloat(data.count) < CGFloat(maxLength) * 0.9 {
+//                min = compression
+//            } else if data.count > maxLength {
+//                max = compression
+//            } else {
+//                break
+//            }
+//        }
+//
+//        return data
+//    }
+    
+}
+
+
